da3db8958ac69c7646bdbf5965a79a8058e30314
IvKonstan <ivanovkonstantin052786@gmail.com>
2026-01-02 02:02:01 +0700

    изменения в main.py

--- Dish-constructor/main.py
+++ Dish-constructor/main.py
@@ -1,5 +1,16 @@
-from auth import AdaptiveMobileLoginApp
+﻿from auth import AdaptiveMobileLoginApp
+import unittest
+import os
+import sys
+import test_runner
+sys.path.insert(0, '.')
+
 
 if __name__ == "__main__":
+    try:
+        unittest.main(module='test_runner', exit=False, verbosity=2)
+    except ImportError as e:
+        print(f"Ошибка: Не найден файл с тестами: {e}")
+
     app = AdaptiveMobileLoginApp()
     app.run()
